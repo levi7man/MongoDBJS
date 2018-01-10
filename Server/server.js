281371d9c6a1513a2a759c649bdd30be41f3c80b
Levi G <levi7man@gmail.com>
2018-01-09 19:43:20 -0600

    Se agrega GET

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,9 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res)=>{
     var todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        completed: req.body.completed
+
     });
     
     todo.save().then((doc)=>{
@@ -21,6 +23,16 @@ app.post('/todos', (req, res)=>{
     });
 });
 
+app.get('/todos', (req, res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    }, (e)=>{
+        res.status(400).send(e);
+    });
+});
+
 app.listen(3000, ()=>{
     console.log('listen in port 3000');
 })
+
+module.exports = {app};
