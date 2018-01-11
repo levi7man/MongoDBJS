0270c31058743b2420038637089e4c39692c1898
Levi G <levi7man@gmail.com>
2018-01-10 22:25:39 -0600

    Se agrega querie para Users

--- Playground/mongose-Queries.js
+++ Playground/mongose-Queries.js
@@ -1,29 +1,46 @@
+
 const {moongose} = require('./../Server/DB/moongose');
 const {Todo} = require('./../Server/models/todo');
+const {user} = require('./../Server/models/user');
 
- var id = '5a558acf4453d8500dfc56d1';
+var idUser = '5a2483222cb7fe981a84500b'
 
-
-
- Todo.find({
-     _id: id
- }).then((todos)=>{
-    console.log('Todos', todos);
- });
-
-
- Todo.findOne({
-    _id: id
-}).then((todo)=>{
-    if (!todo) {
-        return console.log('Id doesnt exist');
-     }
-   console.log('Todo', todo);
+user.findById(idUser).then((users)=>{
+        console.log('User Find By Id', users);
 });
 
-Todo.findById(id).then((todo)=>{
-    if (!todo) {
-       return console.log('Id doesnt exist');
-    }
-    console.log('Todo by ID', todo);
+user.findOne({
+    _id: idUser
+        }).then((users)=>{
+            console.log('User Find ONE', users);
 });
+
+//var id = '5a558acf4453d8500dfc56d1';
+//
+//if (!ObjectID.isValid(id)) {
+//    console.log('Invalid ID');
+//}
+//
+// Todo.find({
+//     _id: id
+// }).then((todos)=>{
+//    console.log('Todos', todos);
+// });
+//
+//
+// Todo.findOne({
+//    _id: id
+//}).then((todo)=>{
+//    if (!todo) {
+//        return console.log('Id doesnt exist');
+//     }
+//   console.log('Todo', todo);
+//});
+//
+//Todo.findById(id).then((todo)=>{
+//    if (!todo) {
+//       return console.log('Id doesnt exist');
+//    }
+//    console.log('Todo by ID', todo);
+//});
+
