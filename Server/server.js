e95cf496342eeb6c274673e2424de068749bb5da
Levi G <levi7man@gmail.com>
2018-01-22 22:40:29 -0600

    Se agrega Token en el Email

--- Server/server.js
+++ Server/server.js
@@ -33,12 +33,14 @@ app.post('/todos', (req, res)=>{
 
 app.post('/users', (req, res)=>{
     var body = _.pick(req.body, ['email', 'password']);
-
     var user = new User(body);
 
-    user.save().then((done)=>{
-        res.send(done);
-    }, (e)=>{
+    user.save().then(()=>{
+        return user.generateAuthToken();
+        //res.send(done);
+    }).then((token)=>{
+        res.header('x-auth', token).send(user);
+    }).catch((e)=>{
         res.status(400).send(e);
     });
 });
